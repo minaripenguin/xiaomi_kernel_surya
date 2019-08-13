a8177a3085b0e09747b10ac0364fd7fd1e85ddaf
Danny Lin <danny@kdrag0n.dev>
2019-08-13 06:13:42 +0000

    sched/boost: Disable sched_boost node functionality
    Causes mass task migrations that we don't want for latency. SchedTune
    boosting does the job better.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- kernel/sched/boost.c
+++ kernel/sched/boost.c
@@ -270,6 +270,7 @@ int sched_boost_handler(struct ctl_table *table, int write,
 		void __user *buffer, size_t *lenp,
 		loff_t *ppos)
 {
+#ifdef 0
 	int ret;
 	unsigned int *data = (unsigned int *)table->data;
 
@@ -288,4 +289,7 @@ int sched_boost_handler(struct ctl_table *table, int write,
 done:
 	mutex_unlock(&boost_mutex);
 	return ret;
+#else
+	return 0;
+#endif
 }
