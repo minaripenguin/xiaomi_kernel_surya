3b7ae60df2563f4fa92734dfa16defb64b2bc2e9
root <alexfinhart@gmail.com>
2021-05-23 16:00:39 +0800

    block: forbid init from changing I/O schedulers and override readahead to 64kb
    Signed-off-by: Alex Finhart <alexfinhart@gmail.com>

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -102,12 +102,12 @@ queue_ra_store(struct request_queue *q, const char *page, size_t count)
 {
 	unsigned long ra_kb;
 	ssize_t ret = queue_var_store(&ra_kb, page, count);
-
+	
 	if (ret < 0)
 		return ret;
 
 	if (!strcmp(current->comm, "init"))
-		ra_kb = VM_MAX_READAHEAD;
+		ra_kb = 64;
 
 	q->backing_dev_info->ra_pages = ra_kb >> (PAGE_SHIFT - 10);
 
