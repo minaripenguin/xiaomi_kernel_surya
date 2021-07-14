579d83d707df140138560bd0963b6898d2df70a8
Yaroslav Furman <yaro330@gmail.com>
2021-07-14 11:10:20 +0300

    Revert "devfreq_boost: Update and expand to handle CPUBW/LLCCBW boosting"
    This reverts commit 43fa18ccf788fc10ed30b2ce4743f75603d7e525.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2250,11 +2250,8 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
-		cpu_input_boost_kick();
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY))
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
-		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
-	}
 
 	drm_modeset_acquire_init(&ctx, 0);
 
